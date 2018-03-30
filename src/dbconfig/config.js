b67713be4bcf111a36522bc74306f0c221db83ec
Garnet Yang <garnetyang@192-168-1-2.tpgi.com.au>
2018-03-30 18:25:39 +1100

    connect to mlab db

--- src/dbconfig/config.js
+++ src/dbconfig/config.js
@@ -7,21 +7,21 @@ export const dbConfig = () => {
     mongoose.Promise = global.Promise;
     
     // // connect to local mongodb : the 'myTestMongodb' will become the db name
-    mongoose.connect('mongodb://localhost:27017/myTestMongodb', { autoIndex: false})
-            .then(() => {
-            console.log('you are connected!');
-            }, (err) => {
-            console.log('[Sorry] - mongodb connection error');
-            });
-    
-    // const user = configValue.user;
-    // const password = configValue.pwd;
-    // const dbConnectString = `mongodb://${user}:${password}@ds163418.mlab.com:63418/mycontact`;
-
-    // mongoose.connect(dbConnectString, {autoIndex: false})
+    // mongoose.connect('mongodb://localhost:27017/myTestMongodb', { autoIndex: false})
     //         .then(() => {
-    //             console.log('connected to mongodb')
+    //         console.log('you are connected!');
     //         }, (err) => {
-    //             console.log(`Sorry - mongodb connection erro ${err}`);
+    //         console.log('[Sorry] - mongodb connection error');
     //         });
+    
+    const user = configValue.user;
+    const password = configValue.pwd;
+    const dbConnectString = `mongodb://${user}:${password}@ds163418.mlab.com:63418/mycontact`;
+
+    mongoose.connect(dbConnectString, {autoIndex: false})
+            .then(() => {
+                console.log('connected to mongodb')
+            }, (err) => {
+                console.log(`Sorry - mongodb connection erro ${err}`);
+            });
 };
