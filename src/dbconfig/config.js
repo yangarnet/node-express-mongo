a84ca48e167d070b74bcee44a9c94f1479ff915a
Garnet Yang <garnetyang@192-168-1-2.tpgi.com.au>
2018-04-09 23:24:18 +1000

    refactor db connection

--- src/dbconfig/config.js
+++ src/dbconfig/config.js
@@ -2,26 +2,30 @@
 import mongoose from 'mongoose';
 import configValue  from './user.json';
 
-export const dbConfig = () => {
+export const dbConfig = (env) => {
     // setup mongo connection
     mongoose.Promise = global.Promise;
     
-    // // connect to local mongodb : the 'myTestMongodb' will become the db name
-    mongoose.connect('mongodb://localhost:27017/myTestMongodb', { autoIndex: false})
+    if (env === 'development') {
+        // connect to local mongodb : the 'myTestMongodb' will become the db name
+        mongoose.connect('mongodb://localhost:27017/myTestMongodb', { autoIndex: false})
             .then(() => {
             console.log('you are connected!');
             }, (err) => {
             console.log('[Sorry] - mongodb connection error');
             });
-    
-    // const user = configValue.user;
-    // const password = configValue.pwd;
-    // const dbConnectString = `mongodb://${user}:${password}@ds163418.mlab.com:63418/mycontact`;
+    }
 
-    // mongoose.connect(dbConnectString, {autoIndex: false})
-    //         .then(() => {
-    //             console.log('connected to mongodb')
-    //         }, (err) => {
-    //             console.log(`Sorry - mongodb connection erro ${err}`);
-    //         });
+    if (env === 'production') {
+        const user = configValue.user;
+        const password = configValue.pwd;
+        const dbConnectString = `mongodb://${user}:${password}@ds163418.mlab.com:63418/myMongodb`;
+    
+        mongoose.connect(dbConnectString, {autoIndex: false})
+            .then(() => {
+                console.log('connected to mongodb')
+            }, (err) => {
+                console.log(`Sorry - mongodb connection erro ${err}`);
+            });
+    }
 };
