3c803fde3aed68259fd411928d8bce3b7455be5f
Garnet Yang <gyang@192-168-1-2.tpgi.com.au>
2018-05-11 16:22:04 +1000

    add some minor changes

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
-import express from 'express';
-import routes from './src/routes/RoutesConfig';
-import { dbConfigure } from './src/dbconfig/config';
-import middleWare from './src/middleware/config';
-import envConfig from './src/config/config.js';
+import express from "express";
+import routes from "./src/routes/RoutesConfig";
+import { dbConfigure } from "./src/dbconfig/config";
+import middleWare from "./src/middleware/config";
+import envConfig from "./src/config/config.js";
 
 const app = express();
 const admin = express();
 
-const dev = 'development';
+const dev = "development";
 const env = process.env.NODE_ENV || dev;
 
 envConfig(env);
@@ -17,25 +17,31 @@ dbConfigure(env);
 middleWare(app, admin);
 routes(app);
 
-app.set('view engine', 'pug');
-app.set('views', './views');
+app.set("view engine", "pug");
+app.set("views", "./views");
 
-app.get('/', (req, res) => {
-    res.end(JSON.stringify(process.env));
-    //res.end(app.get('env'));
+app.get("/", (req, res) => {
+  res.status(200).json({
+    port: process.env.PORT,
+    NODE_ENV: process.env.NODE_ENV
+  });
+  //res.end(app.get('env'));
 });
 
-app.get('/load-view', function (req, res) {
-    res.render('index', { title: 'Hey', message: 'Hello there!, render the view from view template' })
+app.get("/load-view", function(req, res) {
+  res.render("index", {
+    title: "Hey",
+    message: "Hello there!, render the view from view template"
+  });
 });
 
-admin.get('/', (req, res) => {
-    console.log(`admin mountpath path: ${admin.mountpath}`);
-    res.send('welcome to admin page');
+admin.get("/", (req, res) => {
+  console.log(`admin mountpath path: ${admin.mountpath}`);
+  res.send("welcome to admin page");
 });
 
 app.listen(process.env.PORT, () => {
-     console.log(`server running @ port ${process.env.PORT}`);
+  console.log(`server running @ port ${process.env.PORT}`);
 });
 
 // export for supertest
--- src/routes/RoutesConfig.js
+++ src/routes/RoutesConfig.js
@@ -1,57 +1,63 @@
-import * as contacts from '../controllers/ContactsController';
-import todoController from '../controllers/TodoController';
-import userController from '../controllers/UserController';
+import * as contacts from "../controllers/ContactsController";
+import todoController from "../controllers/TodoController";
+import userController from "../controllers/UserController";
 
-const routes = (app) => {
-    // just use the app.route('path').method().......
-    app.route('/contacts')
-       .get((req, res, next) => {
-           // see how to use middleware
-           console.log(`Request from:${req.originalUrl}`);
-           console.log(`Request type:${req.method}`);
-           next();
-        }, contacts.getContacts)
-       //add new contact
-       .post(contacts.addNewContact);
+const routes = app => {
+  // just use the app.route('path').method().......
+  app
+    .route("/contacts")
+    .get((req, res, next) => {
+      // see how to use middleware
+      console.log(`Request from:${req.originalUrl}`);
+      console.log(`Request type:${req.method}`);
+      next();
+    }, contacts.getContacts)
+    //add new contact
+    .post(contacts.addNewContact);
 
-    app.route('/contacts/:contactId')
-       .get(contacts.getContactById)
-       .put(contacts.updateContactById)
-       .delete(contacts.deleteContactById);
+  app
+    .route("/contacts/:contactId")
+    .get(contacts.getContactById)
+    .put(contacts.updateContactById)
+    .delete(contacts.deleteContactById);
 
-    // add a new controller and use it methods
-    const todoCtrl = new todoController();
-    
-    // optimize authentication middleware for all todo routes
-    app.all('/to-do*', userController.authenticate);
+  // add a new controller and use it methods
+  const todoCtrl = new todoController();
 
-    app.route('/to-do')
-       .get(todoCtrl.getTodo)
-       .post([todoController.preSaveMiddleware], todoCtrl.addTodo);
+  // optimize authentication middleware for all todo routes
+  app.all("/to-do*", userController.authenticate);
 
-    app.route('/to-do/:todoId')
-       .get(todoCtrl.getTodoById)
-       .put(todoCtrl.updateTodoById)
-       .delete(todoCtrl.deleteTodoById)
-       .patch(todoCtrl.patchTodoById);
-    
-    app.route('/to-do/:todo')
-       .get(todoCtrl.getTodoByName);
-    
-    const userCtrl = new userController();
+  app
+    .route("/to-do")
+    .get(todoCtrl.getTodo)
+    .post([todoController.preSaveMiddleware], todoCtrl.addTodo);
 
-    app.route('/add-user')
-       .post(userCtrl.addNewUser);
-    
-    // will invoke the middleware for find-me
-    app.get('/find-me', (req,res) => {
-        res.send(req.user);
-    });
+  app
+    .route("/to-do/:todoId")
+    .get(todoCtrl.getTodoById)
+    .put(todoCtrl.updateTodoById)
+    .delete(todoCtrl.deleteTodoById)
+    .patch(todoCtrl.patchTodoById);
 
-    // a new route for user login
-    app.post('/user/login', userCtrl.logIn);
-    // delete user token after logout
-    app.delete('/user/me/token', userController.authenticate, userCtrl.removeToken);
+  app.route("/to-do/:todo").get(todoCtrl.getTodoByName);
+
+  const userCtrl = new userController();
+
+  app.route("/add-user").post(userCtrl.addNewUser);
+
+  // will invoke the middleware for find-me
+  app.get("/find-me", (req, res) => {
+    res.send(req.user);
+  });
+
+  // a new route for user login
+  app.post("/user/login", userCtrl.logIn);
+  // delete user token after logout
+  app.delete(
+    "/user/me/token",
+    userController.authenticate,
+    userCtrl.removeToken
+  );
 };
 
 export default routes;
